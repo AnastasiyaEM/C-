2b0699f5e5ab026b071f6848c583d4e3dd89bb5f
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-02 14:44:47 +0300

    Examples_008_IntroMethod

---
+++ Exampel003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+Console.Write("Введите число n: ");
+int number = Convert.ToInt32(Console.ReadLine());
