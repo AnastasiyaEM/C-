e6520a62d2e32128e35c8f82c7993cf0829fa73e
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-02 14:53:17 +0300

    Exampel010_MethodArray

---
+++ Exampel010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти элемент массива, равный find
+
+int[] array = { 1, 12, 31, 14, 58, 65, 78, 89, 58 };
+
+int n = array.Length;
+int find = 58;
+
+int index = 0;
+
+while (index <  n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
